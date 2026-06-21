11eeb50dbbd486e4c938b553736f10d63a7ffcbd
najelo <44382915+najelo@users.noreply.github.com>
2026-06-21 17:11:10 -0400

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 # Configurar Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 @app.get("/webhook")
 async def verify_webhook(request: Request):
