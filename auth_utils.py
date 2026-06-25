36bf67217e7dfb85dd2f8c0e9e64c3f5e5830b7d
najelo <44382915+najelo@users.noreply.github.com>
2026-06-24 22:28:08 -0400

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -2,6 +2,5 @@ import os
 from supabase import create_client, ClientOptions
 
 def get_supabase():
-    # Aumentamos el timeout a 30 segundos para evitar errores 522 al iniciar
     options = ClientOptions(timeout=30) 
     return create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"), options=options)
